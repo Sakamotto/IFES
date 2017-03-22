ddb063f724704b4a646a7afacdafa69e1365a510
Sakamotto <csanterio@gmail.com>
2017-03-22 16:31:17 -0300

    Tad Hash

---
+++ 6º Período/TPA/TPA20171/src/tadhash/Dado.java
@@ -0,0 +1,27 @@
+package tadhash;
+
+/**
+ * Created by cristian on 22/03/17.
+ */
+public class Dado {
+
+    private String nome;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
---
+++ 6º Período/TPA/TPA20171/src/tadhash/ItemTabHash.java
@@ -0,0 +1,31 @@
+package tadhash;
+
+/**
+ * Created by cristian on 22/03/17.
+ */
+public class ItemTabHash {
+
+    private String key;
+    private Dado dado;
+
+    public ItemTabHash(String key, Dado dado){
+        this.setKey(key);
+        this.setDado(dado);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Dado getDado() {
+        return dado;
+    }
+
+    public void setDado(Dado dado) {
+        this.dado = dado;
+    }
+}
